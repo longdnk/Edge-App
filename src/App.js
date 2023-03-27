2956a95fd10eb1e6fa554e7c39857b3f033f65a7
Thai Nguyen <ThaiNguyenDev15@gmail.com>
2023-03-27 15:26:40 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { DownOutlined } from '@ant-design/icons';
+import { Badge, Dropdown, Space, Table } from 'antd';
+const items = [
+  {
+    key: '1',
+    label: 'Action 1',
+  },
+  {
+    key: '2',
+    label: 'Action 2',
+  },
+];
+const App = () => {
+  const expandedRowRender = (record,index) => {
+    if (index > 0){
+      const columns = [
+        {
+          title: 'Date',
+          dataIndex: 'date',
+          key: 'date',
+        },
+        {
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Status',
+          key: 'state',
+          render: () => <Badge status="success" text="Finished" />,
+        },
+        {
+          title: 'Upgrade Status',
+          dataIndex: 'upgradeNum',
+          key: 'upgradeNum',
+        },
+        {
+          title: 'Action',
+          dataIndex: 'operation',
+          key: 'operation',
+          render: () => (
+            <Space size="middle">
+              <a>Pause</a>
+              <a>Stop</a>
+              <Dropdown
+                menu={{
+                  items,
+                }}
+              >
+                <a>
+                  More <DownOutlined />
+                </a>
+              </Dropdown>
+            </Space>
+          ),
+        },
+      ];
+      const data = [];
+      for (let i = 0; i < 3; ++i) {
+        data.push({
+          key: i.toString(),
+          date: '2014-12-24 23:12:00',
+          name: 'This is production name',
+          upgradeNum: 'Upgraded: 56',
+         
+        });
+      }
+      console.log(index)
+        return <Table columns={columns} dataSource={data} pagination={false} />;
+    } 
+      return null
+    }
+    
+  };
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Platform',
+      dataIndex: 'platform',
+      key: 'platform',
+    },
+    {
+      title: 'Version',
+      dataIndex: 'version',
+      key: 'version',
+    },
+    {
+      title: 'Upgraded',
+      dataIndex: 'upgradeNum',
+      key: 'upgradeNum',
+    },
+    {
+      title: 'Creator',
+      dataIndex: 'creator',
+      key: 'creator',
+    },
+    {
+      title: 'Date',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+    },
+    {
+      title: 'Action',
+      key: 'operation',
+      render: () => <a>Publish</a>,
+    },
+  ];
+  const data = [{
+      key: 1,
+      name: 'Screen',
+      platform: 'iOS',
+      version: '10.3.4.5654',
+      upgradeNum: 500,
+      creator: 'Jack',
+      createdAt: '2014-12-24 23:12:00',
+      children:[
+        {
+          key: 2,
+          date: '2014-12-24 23:12:00',
+          name: 'This is production name',
+          upgradeNum: 'Upgraded: 56',
+        }
+      ]
+  }]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Table
+        columns={columns}
+        expandable={{
+          expandedRowRender,
+          defaultExpandedRowKeys: ['0'],
+        }}
+        dataSource={data}
+      />
+    </>
   );
-}
-
+};
 export default App;
