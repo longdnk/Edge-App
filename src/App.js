5ee60985958225eb2a38e8aad48023367eb30983
longdnk <longdnk@pop-os.localdomain>
2023-04-24 12:57:42 +0700

    form table template

--- src/App.js
+++ src/App.js
@@ -1,164 +1,240 @@
-import { DownOutlined } from '@ant-design/icons';
-import { Badge, Dropdown, Space, Table } from 'antd';
-import { useState } from "react";
+import React, { Component } from "react";
+import { Button, Form, Input, Space, Table, Tag, TreeSelect } from 'antd';
+import "./App.css"
 
-const App = () => {
-    let data = [];
-    const name = ['Jack 5 củ', 'Jack 14 củ', 'Cây ATM']
-    const date = new Date().toLocaleString();
+class App extends Component {
+	formRef = React.createRef();
 
-    const [keyExpand, setKeyExpand] = useState(null);
-    const [rowExpand, setRowExpand] = useState(null);
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: null,
+			clicked: null,
+		}
+	}
 
-    for (let i = 0; i < 3; ++i) {
-        data.push({
-            key: i.toString(),
-            name: 'Screen',
-            platform: 'iOS',
-            version: '10.3.4.5654',
-            upgradeNum: 500,
-            creator: name[i],
-            createdAt: date,
-        });
-    }
+	onClickTable = () => {
+		this.setState({
+			...this.state,
+			clicked: "clicked",
+		})
+	}
 
-    const handleExpand = (rowStatus, rowParams) => {
-        setRowExpand(rowParams.key);
-        setKeyExpand((rowStatus && rowParams.key === rowExpand ? rowParams.key : null));
-    }
+	onChange = value => {
+		this.setState({
+			...this.state,
+			selected: value,
+		})
+	}
 
-    return (
-        <>
-            {
-                keyExpand ?
-                <Table
-                    columns={columns}
-                    expandable={{
-                        expandedRowRender,
-                        expandedRowKeys: [keyExpand],
-                    }}
-                    dataSource={data}
-                    onExpand={handleExpand}
-                />
-                :
-                <Table
-                    columns={columns}
-                    expandable={{
-                        expandedRowRender,
-                        defaultExpandedRowKeys: [keyExpand ?? '1'],
-                    }}
-                    dataSource={data}
-                    onExpand={handleExpand}
-                />
-            }
-        </>
-    );
-};
+	onReset = () => {
+		this.formRef.current.resetFields();
+		this.setState({
+			...this.state,
+			selected: null,
+			clicked: null,
+		})
+	}
 
-const expandedRowRender = (element, index) => {
-    const data = [];
-    const columns = [
-        {
-            title: 'Date',
-            dataIndex: 'date',
-            key: 'date',
-        },
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Status',
-            key: 'state',
-            render: () => <Badge status="success" text="Finished"/>,
-        },
-        {
-            title: 'Upgrade Status',
-            dataIndex: 'upgradeNum',
-            key: 'upgradeNum',
-        },
-        {
-            title: 'Action',
-            dataIndex: 'operation',
-            key: 'operation',
-            render: () => (
-                <Space size="middle">
-                    <a>Pause</a>
-                    <a>Stop</a>
-                    <Dropdown
-                        menu={{
-                            items,
-                        }}
-                    >
-                        <a>
-                            More <DownOutlined/>
-                        </a>
-                    </Dropdown>
-                </Space>
-            ),
-        },
-    ];
+	render = () => {
+		return (
+			<div className={'form-view'}>
+				<h1>Form</h1>
+				<Form
+					name="basic"
+					labelCol={{
+						span: 8,
+					}}
+					wrapperCol={{
+						span: 20,
+					}}
+					style={{
+						maxWidth: 600,
+					}}
+					autoComplete="off"
+					ref={this.formRef}
+				>
+					<Form.Item
+						label="Username"
+						name="username"
+						rules={[
+							{
+								required: true,
+								message: 'Input Something',
+							},
+						]}
+					>
+						<Input/>
+					</Form.Item>
 
-    if (index > 0) {
-        for (let i = 0; i < 3; ++i) {
-            data.push({
-                key: i.toString(),
-                date: '2014-12-24 23:12:00',
-                name: 'This is production name',
-                upgradeNum: 'Upgraded: 56',
-            });
-        }
-    }
-    return <Table columns={columns} dataSource={data} pagination={false}/>;
-};
+					<Form.Item
+						label={"Options"}
+						name={"Options"}
+					>
+						<TreeData
+							{...this}
+							{...this.state}
+						/>
+					</Form.Item>
 
-const items = [
-    {
-        key: '1',
-        label: 'Action 1',
-    },
-    {
-        key: '2',
-        label: 'Action 2',
-    },
-];
+					<Form.Item
+						wrapperCol={{
+							offset: 8,
+							span: 16,
+						}}
+					>
+						<Button type="primary" onClick={this.onReset}>
+							Reset
+						</Button>
+					</Form.Item>
+				</Form>
+				<div>
+					<TableClicked
+						{...this}
+						{...this.state}
+					/>
+				</div>
+			</div>
+		)
+	}
+}
+
+const TreeData = props => {
+	return (
+		<TreeSelect
+			onClick={props.onClickTable}
+			showSearch
+			style={{
+				width: '100%',
+			}}
+			dropdownStyle={{
+				maxHeight: 400,
+				overflow: 'auto',
+			}}
+			placeholder="Variables"
+			allowClear
+			treeDefaultExpandAll
+			onChange={props.onChange}
+			treeData={treeData(props.selected)}
+			defaultValue={props.selected}
+		/>
+	)
+}
+
+const treeData = valueSelected => {
+	return (
+		[
+			{
+				value: 'Inverter 1',
+				title: 'Inverter 1',
+				children: [
+					{
+						value: 'KWH',
+						title: 'KWH'
+					},
+					{
+						value: 'Voltage',
+						title: "Vol"
+					},
+				],
+			},
+			{
+				value: 'Inverter 2',
+				title: 'Inverter 2',
+				children: [
+					{
+						value: 'Ampe',
+						title: 'Ampe',
+					},
+					{
+						value: 'Temperature',
+						title: 'Temperature',
+					},
+				],
+			}
+		]
+	)
+}
+
+const TableClicked = props => {
+	const { clicked } = props;
+	return (
+		<div className={clicked ? "table-clicked" : "table-normal"}>
+			<Table columns={columns} dataSource={data}/>
+		</div>
+	)
+}
 
 const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Platform',
-        dataIndex: 'platform',
-        key: 'platform',
-    },
-    {
-        title: 'Version',
-        dataIndex: 'version',
-        key: 'version',
-    },
-    {
-        title: 'Upgraded',
-        dataIndex: 'upgradeNum',
-        key: 'upgradeNum',
-    },
-    {
-        title: 'Creator',
-        dataIndex: 'creator',
-        key: 'creator',
-    },
-    {
-        title: 'Date',
-        dataIndex: 'createdAt',
-        key: 'createdAt',
-    },
-    {
-        title: 'Action',
-        key: 'operation',
-        render: () => <a>Publish</a>,
-    },
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		key: 'name',
+		render: (text) => <a>{text}</a>,
+	},
+	{
+		title: 'Age',
+		dataIndex: 'age',
+		key: 'age',
+	},
+	{
+		title: 'Address',
+		dataIndex: 'address',
+		key: 'address',
+	},
+	{
+		title: 'Tags',
+		key: 'tags',
+		dataIndex: 'tags',
+		render: (_, { tags }) => (
+			<>
+				{tags.map((tag) => {
+					let color = tag.length > 5 ? 'geekblue' : 'green';
+					if (tag === 'loser') {
+						color = 'volcano';
+					}
+					return (
+						<Tag color={color} key={tag}>
+							{tag.toUpperCase()}
+						</Tag>
+					);
+				})}
+			</>
+		),
+	},
+	{
+		title: 'Action',
+		key: 'action',
+		render: (_, record) => (
+			<Space size="middle">
+				<a>Invite {record.name}</a>
+				<a>Delete</a>
+			</Space>
+		),
+	},
+];
+
+const data = [
+	{
+		key: '1',
+		name: 'John Brown',
+		age: 32,
+		address: 'New York No. 1 Lake Park',
+		tags: ['nice', 'developer'],
+	},
+	{
+		key: '2',
+		name: 'Jim Green',
+		age: 42,
+		address: 'London No. 1 Lake Park',
+		tags: ['loser'],
+	},
+	{
+		key: '3',
+		name: 'Joe Black',
+		age: 32,
+		address: 'Sydney No. 1 Lake Park',
+		tags: ['cool', 'teacher'],
+	},
 ];
 export default App;
